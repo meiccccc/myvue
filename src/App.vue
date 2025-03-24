0ce3cf68bade5832b8d3e3777875955c82d388b5
JZ <youra911@163.com>
2025-03-24 17:06:46 +0800

    feat: 完成用户登33录2功能 (#SCRUM-2)
    - 实现用户登录接口
    - 添加登录页面样式2
    - 更新用户认证逻辑

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <p>测试测试333</p>
       <p>测试测试444</p>
       <p>测试测试555</p>
+      <p>测试测试666</p>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>  
